adcea50bf4f89fc90e0c916d1d6e1466543d4c6d
Cristian184 <97376365+Cristian184@users.noreply.github.com>
2025-05-30 12:23:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const displayArea = document.getElementById('display-area');
 characterBtn.addEventListener('click', fetchCharacter);
 planetBtn.addEventListener('click', fetchPlanet);
 
+
 async function fetchCharacter() {
+    
   try {
+    displayArea.classList.remove('hidden');
     const id = Math.floor(Math.random() * 83) + 1;
     const response = await fetch(`https://swapi.tech/api/people/${id}`);
     const data = await response.json();
@@ -18,13 +21,17 @@ async function fetchCharacter() {
       <p><strong>Gender:</strong> ${char.gender}</p>
     `;
   } catch (err) {
+    displayArea.classList.remove('hidden');
     displayArea.innerHTML = `<p>Error loading character. Please try again.</p>`;
     console.error(err);
   }
 }
 
+
 async function fetchPlanet() {
+    
   try {
+    displayArea.classList.remove('hidden');
     const id = Math.floor(Math.random() * 60) + 1;
     const response = await fetch(`https://swapi.tech/api/planets/${id}`);
     const data = await response.json();
@@ -36,6 +43,7 @@ async function fetchPlanet() {
       <p><strong>Population:</strong> ${planet.population}</p>
     `;
   } catch (err) {
+    displayArea.classList.remove('hidden');
     displayArea.innerHTML = `<p>Error loading planet. Please try again.</p>`;
     console.error(err);
   }
